28592f48c93106d7917682c01d8f154d8eae1dd6
ZackaryWelch <welch.zackary@gmail.com>
2018-08-08 10:32:07 +0000

    Ejercicio 2

--- U00_Repaso/Ej-02/main.cpp
+++ U00_Repaso/Ej-02/main.cpp
@@ -4,6 +4,24 @@
 using namespace std;
 
 int main() {
+    Calculadora<int, long> micalc;
+    Calculadora<float, double> micalcDouble;
     cout << "Ejercicio 00/02\n" << endl;
+    cout << micalc.sumar(2,2) << endl;
+    cout << micalc.restar(2,3) << endl;
+    cout << micalc.dividir(4,2) << endl;
+    cout << micalc.sumar(2,3) << endl;
+
+    try {
+        cout << micalc.sumar(4,0) << endl;
+    } catch (int e) {
+        cout << "error: " << e << endl;
+    }
+
+
+    cout << micalcDouble.sumar(2,2) << endl;
+    cout << micalcDouble.restar(2,3) << endl;
+    cout << micalcDouble.dividir(5,2) << endl;
+    cout << micalcDouble.sumar(2,3) << endl;
     return 0;
 }
