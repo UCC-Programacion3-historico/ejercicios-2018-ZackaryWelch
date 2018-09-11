4de4be73a39c94c3e6ca1b90a80490d1a4dd085e
ZackaryWelch <welch.zackary@gmail.com>
2018-09-11 14:23:14 -0300

    Terminado con U00/Ej-04

--- U00_Repaso/Ej-04/main.cpp
+++ U00_Repaso/Ej-04/main.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 
 int main() {
+	int segundosE, horas, minutos, segundosR;
     cout << "Ejercicio 00/04\n" << endl;
+
+	cout << "Entrae segundos: ";
+	cin >> segundosE;
+
+	horas = segundosE / 3600;
+	minutos = (segundosE % 3600) / 60;
+	segundosR = segundosE % 60;
+
+	cout << segundosE << " segundos equivalen a: " << horas << " horas, " << minutos << " minutos y " << segundosR << " segundos.";
+
     return 0;
 }
