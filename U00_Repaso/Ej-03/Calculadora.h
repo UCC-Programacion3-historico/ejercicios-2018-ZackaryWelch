267daf87969378b354d317f4ff50f9d975bec8d8
ZackaryWelch <welch.zackary@gmail.com>
2018-09-11 14:13:43 -0300

    Terminado con U00/Ej-03

--- U00_Repaso/Ej-03/Calculadora.h
+++ U00_Repaso/Ej-03/Calculadora.h
@@ -1,9 +1,18 @@
 #ifndef REPASO_CALCULADORA_H
 #define REPASO_CALCULADORA_H
 
+#include <iostream>
 
+using namespace std;
+
+template<class T, class P>
 class Calculadora {
-
+public:
+    T sumar(T a, T b);
+    T restar(T a, T b);
+    T dividir(T a, T b);
+    P multiplicar(T a, T b);
+    bool igual(T a, T b);
 };
 
 
--- U00_Repaso/Ej-03/main.cpp
+++ U00_Repaso/Ej-03/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 #include "Calculadora.h"
+#include "Calculadora.cpp"
 
 using namespace std;
 
 int main() {
-    cout << "Ejercicio 00/03\n" << endl;
+    Calculadora<int, long> micalc;
+    Calculadora<float, double> micalcDouble;
+    cout << "Ejercicio 00/02\n" << endl;
+    cout << micalc.sumar(2,2) << endl;
+    cout << micalc.restar(2,3) << endl;
+    cout << micalc.dividir(4,2) << endl;
+    cout << micalc.multiplicar(2,3) << endl;
+
+    try {
+        cout << micalc.dividir(4,0) << endl;
+    } catch (int e) {
+        cout << "error: " << e << endl;
+    }
+
+
+    cout << micalcDouble.sumar(2,2) << endl;
+    cout << micalcDouble.restar(2,3) << endl;
+    cout << micalcDouble.dividir(5,2) << endl;
+    cout << micalcDouble.multiplicar(2,3) << endl;
     return 0;
 }
