52fee892cb7817f760d92aa10ff1b66cd58016d7
ZackaryWelch <welch.zackary@gmail.com>
2018-09-12 00:28:33 -0300

    Terminado con U00/Ej-06 sin testing

--- U00_Repaso/Ej-06/main.cpp
+++ U00_Repaso/Ej-06/main.cpp
@@ -5,13 +5,19 @@ using namespace std;
 
 int main() {
     clock_t begin;
-    begin = clock();
-
     cout << "Comenzando a medir Tiempo\n" << endl;
+
+    begin = clock();
+	
+	int a[1024];
+	for(int i = 0; i < 1024; i++) {
+		a[i] = i;
+	}
+
     clock_t end = clock();
 
     double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
 
-    cout << "Tardo elapsed_secs" << elapsed_secs << "\n" << std::endl;
+    cout << "Tardo elapsed_secs: " << elapsed_secs << "\n" << std::endl;
     return 0;
 }
