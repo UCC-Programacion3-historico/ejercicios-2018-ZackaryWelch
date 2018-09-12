54befc82fa2dbba3df637ff991924d26af3e0bbf
ZackaryWelch <welch.zackary@gmail.com>
2018-09-12 00:51:04 -0300

    Terminado con U01/Ej-02

--- U01_Recursividad/Ej-02/main.cpp
+++ U01_Recursividad/Ej-02/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 #include "sumatoria.h"
 
+using namespace std;
+
 int main() {
-    std::cout << "Ejercicio 01/02\n" << std::endl;
+    cout << "Ejercicio 01/02\n" << endl;
+	int size;
+	cout << "Tamaño del array: ";
+	cin >> size;
+	
+	if(size > 0) {
+		int a[size];
+		cout << "Entre elementos: ";
+		for(int i = 0; i < size; i++) {
+			int num;
+			cin >> num;
+			a[i] =  num;
+		}
+		cout << "Suma del elementos del array: " << sumatoria(a, size);
+	}else{
+		cout << "Tamaño invalido.";
+	}
+
     return 0;
 }
--- U01_Recursividad/Ej-02/sumatoria.cpp
+++ U01_Recursividad/Ej-02/sumatoria.cpp
@@ -2,5 +2,8 @@
 
 
 int sumatoria(int *arr, unsigned int size) {
-    return 0;
+	if(size > 0)
+		return arr[size-1]+sumatoria(arr, size-1);
+	else
+		return 0;
 }
