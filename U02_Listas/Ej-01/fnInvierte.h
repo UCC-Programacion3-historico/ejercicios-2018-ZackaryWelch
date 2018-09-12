4a091dc5ee3171cf71e575d7e4e03b0ba8d769eb
ZackaryWelch <welch.zackary@gmail.com>
2018-09-12 12:00:51 -0300

    Progreso con U02/Ej-01

--- U02_Listas/Ej-01/fnInvierte.h
+++ U02_Listas/Ej-01/fnInvierte.h
@@ -9,7 +9,13 @@ void fnInvierte (Lista<T> *lis);
 
 template <class T>
 void fnInvierte (Lista<T> *lis){
+	int tamanio = lis->getTamanio() - 1;
 
+	for(int i = 0; i < lis->getTamanio() / 2; i++) {
+		T pri = lis->getDato(i);
+		lis->reemplazar(i, lis->getDato(tamanio - i));
+		lis->reemplazar(tamanio - i, pri);
+	}
 }
 
 
--- U02_Listas/Ej-01/main.cpp
+++ U02_Listas/Ej-01/main.cpp
@@ -4,5 +4,25 @@
 
 int main() {
     std::cout << "Ejercicio 02/01\n" << std::endl;
+
+	std::cout << "Original:" << std::endl;
+	Lista<int> *lista = new Lista<int>();
+	for(int i = 10; i < 20; i++) {
+		lista->insertarPrimero(i);
+		std::cout << i;
+		if(i != 19)
+			std::cout << "->";
+	}
+	std::cout << '\n';
+	fnInvierte(lista);
+
+	std::cout << "Invertido:" << std::endl;
+	for(int i = 0; i < lista->getTamanio(); i++) {
+		std::cout << lista->getDato(i);
+		if(i != lista->getTamanio() - 1)
+			std::cout << "->";
+	}
+ 	std::cout << '\n';
+
     return 0;
 }
