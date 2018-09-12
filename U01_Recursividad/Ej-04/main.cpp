0f3b50b255ae017cd79569a2b09bee7f9f38a30e
ZackaryWelch <welch.zackary@gmail.com>
2018-09-12 11:27:49 -0300

    Terminado con U01/Ej-04

--- U01_Recursividad/Ej-04/main.cpp
+++ U01_Recursividad/Ej-04/main.cpp
@@ -3,5 +3,9 @@
 
 int main() {
     std::cout << "Ejercicio 01/04\n" << std::endl;
+
+	std::cout << "MCD(54, 24): " << mcd(54, 24) << std::endl;
+	std::cout << "MCD(42, 56): " << mcd(42, 56) << std::endl;
+	std::cout << "MCD(62, 36): " << mcd(62, 36) << std::endl;
     return 0;
 }
