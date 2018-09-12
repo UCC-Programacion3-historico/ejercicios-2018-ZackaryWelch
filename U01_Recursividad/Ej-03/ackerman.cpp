176491bc40d6ff4af041dd4ae15e3742d0ba2d0a
ZackaryWelch <welch.zackary@gmail.com>
2018-09-12 09:45:40 -0300

    Terminado con U01/Ej-03, pero no funciona con m > 3

--- U01_Recursividad/Ej-03/ackerman.cpp
+++ U01_Recursividad/Ej-03/ackerman.cpp
@@ -1,6 +1,11 @@
 #include "ackerman.h"
-
+#include <iostream>
 
 unsigned int ackerman(unsigned int m, unsigned int n) {
-    return 0;
+	if(m == 0)
+		return n+1;
+	else if(m > 0 and n == 0)
+		return ackerman(m-1, 1);
+	else 
+		return ackerman(m-1, ackerman(m, n-1));
 }
--- U01_Recursividad/Ej-03/main.cpp
+++ U01_Recursividad/Ej-03/main.cpp
@@ -2,6 +2,13 @@
 #include "ackerman.h"
 
 int main() {
+	//No puede funcionar si m > 3
     std::cout << "Ejercicio 01/03\n" << std::endl;
+	std::cout << "A(1, 3): " << ackerman(1, 3) << std::endl;
+	std::cout << "A(2, 5): " << ackerman(2, 5) << std::endl;
+	std::cout << "A(0, 8): " << ackerman(0, 8) << std::endl;
+	std::cout << "A(3, 10): " << ackerman(3, 10) << std::endl;
+	//No va a retornar
+	//std::cout << "A(4, 3): " << ackerman(4, 3) << std::endl;
     return 0;
 }
