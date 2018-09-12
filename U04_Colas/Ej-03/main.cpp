c54accae58e4a65f1f8d7350b91f703af9249770
ZackaryWelch <welch.zackary@gmail.com>
2018-09-12 10:45:35 -0300

    Terminado (pienso) con U04/Ej-03

--- U04_Colas/Ej-03/main.cpp
+++ U04_Colas/Ej-03/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
-#include "../Cola/Cola.h"
+#include "ColaPrioridades.h"
 
 int main() {
     std::cout << "Ejercicio 04/03\n" << std::endl;
+
+	ColaPrioridades<int> c;
+
+	for(int i = 10; i < 20; i++)
+		c.encolar(i, i % 3);
+	while(!c.esVacia())
+		std::cout << c.desencolar() << std::endl;
     return 0;
 }
