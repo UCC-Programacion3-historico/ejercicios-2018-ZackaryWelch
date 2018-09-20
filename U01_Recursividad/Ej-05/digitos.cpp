e15856c8a7ac7d8e47805bbcd75033b6711d9d37
ZackaryWelch <welch.zackary@gmail.com>
2018-09-20 17:08:22 -0300

    Terminado con U01/Ej-05

--- U01_Recursividad/Ej-05/digitos.cpp
+++ U01_Recursividad/Ej-05/digitos.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
+#include <cmath>
 #include "digitos.h"
 
-using namespace std;
-
 unsigned int cociente(unsigned int a, unsigned int b) {
-    return 0;
+    if(b == 0)
+        throw 1;
+    if(a < b)
+        return 0;
+    return cociente(a - b, b) + 1;
 }
-
 
 unsigned int resto(unsigned int a, unsigned int b) {
-    return 0;
+    if(b == 0)
+        throw 1;
+    if(a == b)
+        return 0;
+    else if(a > b)
+        return resto(a - b, b);
+    return a;
 }
 
-
 void escribir_espaciado(unsigned int num) {
+    int numDigits = (num == 0) ? 0 : (int)log10(num);
+    if(numDigits == 0) {
+        std::cout << num << " ";
+    }else{
+        std::cout << cociente(num, pow(10, numDigits)) << " ";
+        unsigned int newNum = resto(num, pow(10, numDigits));
+        if((int)log10(newNum) != numDigits - 1){
+            int num_ceros = numDigits - (int)log10(newNum);
+            for(int i = 0; i < num_ceros - 1; i++)
+                std::cout << 0 << " ";
+        }
+        escribir_espaciado(resto(num, pow(10, numDigits)));
+    }
 }
--- U01_Recursividad/Ej-05/main.cpp
+++ U01_Recursividad/Ej-05/main.cpp
@@ -3,5 +3,15 @@
 
 int main() {
     std::cout << "Ejercicio 01/05\n" << std::endl;
+    escribir_espaciado(1024);
+    std::cout << std::endl;
+    escribir_espaciado(10);
+    std::cout << std::endl;
+    escribir_espaciado(8324);
+    std::cout << std::endl;
+    escribir_espaciado(515234);
+    std::cout << std::endl;
+    escribir_espaciado(654321);
+    std::cout << std::endl;
     return 0;
 }
