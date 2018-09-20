77b4db7375477d543997c139792b672b0150b8b5
ZackaryWelch <welch.zackary@gmail.com>
2018-09-20 17:27:58 -0300

    Terminado con U03/Ej-01

--- U03_Pilas/Ej-01/main.cpp
+++ U03_Pilas/Ej-01/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <string>
 #include "../Pila/Pila.h"
 
+using namespace std;
+
 int main() {
-    std::cout << "Ejercicio 03/01\n" << std::endl;
+    string palabra;
+    Pila<char> p;
+    cout << "Ejercicio 03/01\n" << endl;
+    cout << "Entre una palabra: ";
+    getline(cin, palabra);
+    for(int i = 0; i < palabra.length(); i++)
+        p.push(palabra[i]);
+    while(!p.esVacia()) {
+        char c = p.pop();
+        cout << c;
+    }
+    cout << endl;
+    
     return 0;
 }
