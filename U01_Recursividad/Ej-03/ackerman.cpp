549bec14289e5368009a6f1a65fd62b69d6d53fb
ZackaryWelch <welch.zackary@gmail.com>
2018-09-20 15:20:21 -0300

    Simplifiqué ackerman.cpp, pero todavía no funciona con (4,1) y (5,0)

--- U01_Recursividad/Ej-03/ackerman.cpp
+++ U01_Recursividad/Ej-03/ackerman.cpp
@@ -4,8 +4,7 @@
 unsigned int ackerman(unsigned int m, unsigned int n) {
 	if(m == 0)
 		return n+1;
-	else if(m > 0 and n == 0)
+	if(n == 0)
 		return ackerman(m-1, 1);
-	else 
-		return ackerman(m-1, ackerman(m, n-1));
+	return ackerman(m-1, ackerman(m, n-1));
 }
