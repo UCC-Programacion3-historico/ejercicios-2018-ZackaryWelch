326017c96ac5fa6b95373eb86da3bcff4459dfa1
ZackaryWelch <welch.zackary@gmail.com>
2018-09-26 11:47:12 -0300

    Progreso con U04/Ej-02

--- U04_Colas/Ej-02/main.cpp
+++ U04_Colas/Ej-02/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 #include "../Cola/Cola.h"
+#include "../../U03_Pilas/Pila/Pila.h"
+
+using namespace std;
+
+bool palindrome(string p) {
+    Cola<char> cola;
+    Pila<char> pila;
+
+    
+}
 
 int main() {
-    std::cout << "Ejercicio 04/02\n" << std::endl;
+    string p;
+
+    cout << "Ejercicio 04/02\n" << endl;
+    cout << "Entre una frase: ";
+    getline(cin, p);
+
+    cout << (palindrome(p) ? "Sí, es un palidromo" : "No, no es un palidromo") << endl;
+    
     return 0;
 }
