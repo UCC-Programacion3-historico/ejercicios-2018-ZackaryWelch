c5fd8c9abe8ccf0aba0f22754822759439beb206
ZackaryWelch <welch.zackary@gmail.com>
2018-09-26 10:52:09 -0300

    Terminado con U03/Ej-04

--- U03_Pilas/Ej-04/main.cpp
+++ U03_Pilas/Ej-04/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <string>
 #include "../Pila/Pila.h"
 
+using namespace std;
+
 int main() {
-    std::cout << "Ejercicio 03/04\n" << std::endl;
+    string funcion;
+    Pila<char> p;
+    
+    cout << "Ejercicio 03/04\n" << endl;
+    cout << "Entre una función: ";
+    getline(cin, funcion);
+    
+    for(int i = 0; i < funcion.length(); i++)
+        p.push(funcion[i]);
+    
+    int open=0, close=0;
+    while(!p.esVacia()) {
+        char c = p.pop();
+        open+=(c == '{' or c == '(' or c == '[');
+        close+=(c == '}' or c == ')' or c == ']');
+    }
+    
+    if(open == close)
+        cout << "MISMO" << endl;
+    else
+        cout << ((open > close) ? "Más abierto" : "Más cerrado") << endl;
+    
     return 0;
 }
