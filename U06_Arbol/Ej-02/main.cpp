89fe9e2d487ce12a316a674a8141bba214c6ea63
ZackaryWelch <welch.zackary@gmail.com>
2018-10-03 12:01:26 -0300

    Progresso con U06/Ej-02

--- U06_Arbol/Ej-02/main.cpp
+++ U06_Arbol/Ej-02/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include "../Arbol/ArbolBinario.h"
+
+using namespace std;
 
 int main() {
-    std::cout << "Ejercicio 05/02\n" << std::endl;
+    ArbolBinario<int> *a = new ArbolBinario<int>();
+    ArbolBinario<int> *b = new ArbolBinario<int>();
+    int at[9] = {9, 3, 7, 5, 1, 2, 6, 8, 4};
+    int bt[13] = {5, 6, 12, 10, 1, 9, 13, 4, 8, 2, 7, 3, 11};
+    for(int i = 0; i < 9; i++)
+        a->put(at[i]);
+    for(int i = 0; i < 13; i++)
+        b->put(bt[i]);
+    cout << "A preorden: ";
+    a->preorder();
+    cout << endl;
+
+    cout << "A inorden: ";
+    a->inorder();
+    cout << endl;
+
+    cout << "B inorden: ";
+    b->inorder();
+    cout << endl;
+
+    cout << "B postorden: ";
+    b->postorder();
+    cout << endl;
+
+    cout << "Ejercicio 05/02\n" << endl;
     return 0;
 }
