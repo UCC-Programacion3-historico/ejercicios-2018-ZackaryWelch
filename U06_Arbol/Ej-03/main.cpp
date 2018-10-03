6363961fad4ac93532e89f1d77b21d2b5bbfceb4
ZackaryWelch <welch.zackary@gmail.com>
2018-10-03 12:03:42 -0300

    Progreso con U06/Ej-03

--- U06_Arbol/Ej-03/main.cpp
+++ U06_Arbol/Ej-03/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include "../Arbol/ArbolBinario.h"
+
+using namespace std;
+
+int contarPorNivel(ArbolBinario<int> &a, int nivel) {
+
+}
 
 int main() {
-    std::cout << "Ejercicio 05/03\n" << std::endl;
+    ArbolBinario a<int>;
+    for(int i = 0; i < 25; i++)
+        a.put(i);
+
+    cout << "Ejercicio 05/03\n" << endl;
+
+    for(int i = 0; i < 10; i++) {
+        cout << "Nivel " << i << ": " << contarPorNivel(a, i) << endl;
+    } 
     return 0;
 }
