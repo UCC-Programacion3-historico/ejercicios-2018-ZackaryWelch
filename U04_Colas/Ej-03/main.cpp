5949e93e8da6f3b70a53629bcbeff0473e36b058
ZackaryWelch <welch.zackary@gmail.com>
2018-10-04 15:42:23 -0300

    Terminado con U04/Ej-03

--- U04_Colas/Ej-03/main.cpp
+++ U04_Colas/Ej-03/main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 #include "ColaPrioridades.h"
 
+using namespace std;
+
 int main() {
-    std::cout << "Ejercicio 04/03\n" << std::endl;
+    cout << "Ejercicio 04/03\n" << endl << "Termas: ";
 
 	ColaPrioridades<int> c;
 
-	for(int i = 10; i < 20; i++)
+	for(int i = 10; i < 20; i++) {
+        int j = i % 3;
 		c.encolar(i, i % 3);
+        cout << i << ' ';
+    }
+    cout << endl << "Desencolado: ";
 	while(!c.esVacia())
-		std::cout << c.desencolar() << std::endl;
+		cout << c.desencolar() << ' ';
+    cout << endl;
+
     return 0;
 }
