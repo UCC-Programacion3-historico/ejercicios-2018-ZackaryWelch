d0a129b05d73f7b2763f482e7f87875e670de414
ZackaryWelch <welch.zackary@gmail.com>
2018-10-24 11:16:55 -0300

    Actualmente terminado con U05/Ej-01

--- U05_Hash/Ej-01/main.cpp
+++ U05_Hash/Ej-01/main.cpp
@@ -1,6 +1,30 @@
-#include <iostream>
+ #include <iostream>
+#include <string>
+#include "../HashMap/HashMap.h"
+
+//Tamano de elementos
+#define TAM 5
+//Tamano de hash
+#define TAMH 17
+
+using namespace std;
+
+unsigned int hashFunc(int clave) {
+    return clave % TAMH;
+}
 
 int main() {
-    std::cout << "Ejercicio 05/01\n" << std::endl;
-    return 0;
+    string s[TAM] = {"John Smith", "Lisa Smith", "Sandra Dee", "Ted Baker", "Sam Doe"};
+    int claves[TAM] = {59, 12, 72, 92, 14};
+    //Tenia que modificar el tamano del hash (TAM->TAMH) pq habia colisiones con tamano de TAM
+    HashMap<int, string> hash(TAMH, &hashFunc);
+     std::cout << "Ejercicio 05/01\n" << std::endl;
+
+    for(int i = 0; i < TAM; i++)
+        hash.put(claves[i], s[i]);
+
+    for(int i = 0; i < TAM; i++)
+        cout << "Posicion: " << i+1 << "\tClave: " << claves[i] << "\t Valor: " << hash.get(claves[i]) << endl;
+
+     return 0;
 }
