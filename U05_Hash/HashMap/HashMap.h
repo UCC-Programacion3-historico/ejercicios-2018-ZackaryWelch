ada74d24fe5f6513518bfd4480fbe7ab391bb900
ZackaryWelch <welch.zackary@gmail.com>
2018-10-24 11:13:23 -0300

    Agregado HashMap y terminado con U05/Ej-01

--- U05_Hash/HashMap/HashMap.h
+++ U05_Hash/HashMap/HashMap.h
@@ -6,8 +6,13 @@ class HashMap {
 private:
     unsigned int hashFunc(K clave);
 
+    static unsigned int hashFuncDefault(K clave) { return 0; };
+
     unsigned int (*hashFuncP)(K clave);
 
+    int tam;
+
+    T **datos;
 
 public:
     HashMap(unsigned int k);
@@ -28,28 +33,49 @@ public:
 
 template<class K, class T>
 HashMap<K, T>::HashMap(unsigned int k) {
-
+    datos = new T *[k];
+    tam = k;
+    hashFuncP = this->hashFuncDefault;
+    for(int i = 0; i < k; i++)
+        datos[i] = nullptr;
 }
 
 template<class K, class T>
 HashMap<K, T>::~HashMap() {
-
+    for(int i = 0; i < tam; i++)
+        if(datos[i] != nullptr)
+            delete datos[i];
 }
 
 template<class K, class T>
 T HashMap<K, T>::get(K clave) {
-    T temp;
-    return temp;
+    unsigned int idx = hashFunc(clave);
+
+    if(datos[idx] == nullptr)
+        throw 404;
+
+    return *datos[idx];
 }
 
 template<class K, class T>
 void HashMap<K, T>::put(K clave, T valor) {
+    unsigned int idx = hashFunc(clave);
 
+    if(datos[idx] != nullptr)
+        throw 1;
+
+    datos[idx] = new T;
+    *datos[idx] = valor; 
 }
 
 template<class K, class T>
 void HashMap<K, T>::remove(K clave) {
+    unsigned int idx = hashFunc(clave);
 
+    if(datos[idx] == nullptr)
+        throw 404;
+
+    delete datos[idx];
 }
 
 template<class K, class T>
@@ -59,13 +85,16 @@ bool HashMap<K, T>::esVacio() {
 
 template<class K, class T>
 unsigned int HashMap<K, T>::hashFunc(K clave) {
-    return 99999;
+    return hashFuncP(clave) % tam;
 }
 
 template<class K, class T>
 HashMap<K, T>::HashMap(unsigned int k, unsigned int (*fp)(K)) {
-
+    datos = new T *[k];
+    tam = k;
+    hashFuncP = fp;
+    for(int i = 0; i < k; i++)
+        datos[i] = nullptr;
 }
-
 
 #endif //HASHMAP_H
