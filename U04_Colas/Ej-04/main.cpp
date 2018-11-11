e9f5f3ed5e065fbaea635ff85f49e09dd2248ced
ZackaryWelch <welch.zackary@gmail.com>
2018-11-11 17:17:47 -0300

    Terminado con U04/Ej-04

--- U04_Colas/Ej-04/main.cpp
+++ U04_Colas/Ej-04/main.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
-#include "../Cola/Cola.h"
+#include "ColaPrioridades.h"
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+struct Producto {
+    //0 == si
+    bool embarazada;
+    int cantidad;
+    int precio;
+};
 
 int main() {
-    std::cout << "Ejercicio 04/04\n" << std::endl;
+    cout << "Ejercicio 04/04\n" << endl;
+    ColaPrioridades<struct Producto> caja;
+    ifstream file("productos.txt");
+    int cantidades=0, precios=0, embarazadas=0;
+    string line;
+
+    if(file.is_open()) {
+        while(getline(file, line)) {
+            struct Producto producto;
+            file >> producto.embarazada >> producto.cantidad >> producto.precio;
+            caja.encolar(producto, producto.embarazada);
+        }
+    }
+    
+    while(!caja.esVacia()) {
+        struct Producto producto = caja.desencolar();
+        cantidades += producto.cantidad;
+        precios += producto.precio;
+        embarazadas += producto.embarazada;
+    }
+
+    cout << "Cantidad de productos: " << cantidades << endl;
+    cout << "Gasto total: " << precios << endl;
+    cout << "Embarazadas servidas: " << embarazadas << endl;
     return 0;
 }
