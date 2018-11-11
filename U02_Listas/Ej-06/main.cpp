e097d70be57e1f8be8432f2e7c7742f3fe857452
ZackaryWelch <welch.zackary@gmail.com>
2018-11-11 16:31:28 -0300

    Added main.cpp for U02/Ej-06

--- U02_Listas/Ej-06/main.cpp
+++ U02_Listas/Ej-06/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include "../Lista/Lista.h"
 
+using namespace std;
+
 int main() {
-    std::cout << "Ejercicio 01/05\n" << std::endl;
+    cout << "Ejercicio 01/05\n" << endl;
+    Lista<int> lista;
+    lista.insertarUltimo(1);
+    lista.insertarUltimo(2);
+    lista.insertarUltimo(1);
+    lista.insertarUltimo(3);
+    lista.insertarUltimo(1);
+    lista.insertarUltimo(8);
+    lista.insertAfter2(1, 2, 15);
+    cout << lista << endl;
     return 0;
 }
