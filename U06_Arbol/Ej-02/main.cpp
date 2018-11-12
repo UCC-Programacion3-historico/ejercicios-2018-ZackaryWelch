a64c439992ce7d9e597b562e0c67db7620711034
ZackaryWelch <welch.zackary@gmail.com>
2018-11-12 13:31:47 -0300

    Beginning work on U06/Ej-02

--- U06_Arbol/Ej-02/main.cpp
+++ U06_Arbol/Ej-02/main.cpp
@@ -3,21 +3,50 @@
 
 using namespace std;
 
+int find(int *a, int s, int e, int v) {
+    for(int i = s; i < e; i++) {
+        if(a[i] == v)
+            return i;
+    }
+}
+
+void buildArbolA(ArbolBinario<int> *a, int *ain, int *apre) {
+    int inOrderIndex = find(ain, 0, 9, apre[0]);
+}
+
+void buildArbolB(ArbolBinario<int> *b, int* bin, int* bpost) {
+    int inOrderIndex = find(bin, 0, 13, bpost[12]);
+}
+
 int main() {
+    int apre[9] = {2,5,3,9,7,1,6,4,8};
+    int ain[9] = {9,3,7,5,1,2,6,8,4}; 
+    int bin[13] = {5,6,12,10,1,9,13,4,8,2,7,3,11};
+    int bpost[13] = {6,5,10,9,1,13,12,2,8,3,11,7,4};
+
     ArbolBinario<int> *a = new ArbolBinario<int>();
+    a->put(apre[0]);
     ArbolBinario<int> *b = new ArbolBinario<int>();
-    int at[9] = {9, 3, 7, 5, 1, 2, 6, 8, 4};
-    int bt[13] = {5, 6, 12, 10, 1, 9, 13, 4, 8, 2, 7, 3, 11};
-    for(int i = 0; i < 9; i++)
-        a->put(at[i]);
-    for(int i = 0; i < 13; i++)
-        b->put(bt[i]);
+    b->put(bpost[12]);
+    buildArbolA(a, ain, apre);
+    buildArbolB(b, bin, bpost);
+
+    cout << "Ejercicio 05/02\n" << endl;
+
     cout << "A preorden: ";
     a->preorder();
     cout << endl;
 
     cout << "A inorden: ";
     a->inorder();
+    cout << endl;
+
+    cout << "A postorden*: ";
+    a->postorder();
+    cout << endl << endl;
+
+    cout << "B preorden*: ";
+    b->preorder();
     cout << endl;
 
     cout << "B inorden: ";
@@ -28,6 +57,5 @@ int main() {
     b->postorder();
     cout << endl;
 
-    cout << "Ejercicio 05/02\n" << endl;
     return 0;
 }
