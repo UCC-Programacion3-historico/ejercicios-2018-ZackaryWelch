800c53305303dee0098f53b6385430fb04dfd143
ZackaryWelch <welch.zackary@gmail.com>
2018-11-12 13:54:32 -0300

    Terminado con U06/Ej-03

--- U06_Arbol/Ej-03/main.cpp
+++ U06_Arbol/Ej-03/main.cpp
@@ -1,21 +1,39 @@
 #include <iostream>
 #include "../Arbol/ArbolBinario.h"
+#include <ctime>
+#include <cstdlib>
+#include <vector>
 
 using namespace std;
 
-int contarPorNivel(ArbolBinario<int> &a, int nivel) {
-
+bool find(vector<int> &buf, int v) {
+    for(int i = 0; i < buf.size(); i++) {
+        if(buf[i] == v)
+            return true;
+    }
+    return false;
 }
 
 int main() {
-    ArbolBinario a<int>;
-    for(int i = 0; i < 25; i++)
-        a.put(i);
+    ArbolBinario<int> *a = new ArbolBinario<int>();
+    srand(time(NULL));
+    //A buffer to make sure no duplicate numbers are added to the tree
+    vector<int> buf;
+    for(int i = 0; i < 25; i++) {
+        int num = rand() % 4000 + 200;
+        //Gets a new number if a duplicate is detected
+        if(find(buf, num)) {
+            i--;
+            continue;
+        }
+        buf.push_back(num);
+        a->put(num);
+    }
 
     cout << "Ejercicio 05/03\n" << endl;
 
-    for(int i = 0; i < 10; i++) {
-        cout << "Nivel " << i << ": " << contarPorNivel(a, i) << endl;
-    } 
+    for(int i = 0; i < 10; i++)
+        cout << "Nivel " << i << ": " << a->contarPorNivel(i) << endl;
+
     return 0;
 }
