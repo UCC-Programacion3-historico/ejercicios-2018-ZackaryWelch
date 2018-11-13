c3ab814ec092a0efe575c51e5aa0f310d3bb6de6
ZackaryWelch <welch.zackary@gmail.com>
2018-11-12 23:03:51 -0300

    Terminado con U08/Ej-04

--- U08_Ordenamiento/Ej-04/main.cpp
+++ U08_Ordenamiento/Ej-04/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void shellSort(int *a, int n) {
+    for (int k = n/2; k > 0; k /= 2) { 
+        for (int i = k; i < n; i++) { 
+            int temp = a[i]; 
+            int j;
+            for (j = i; j >= k and a[j - k] > temp; j -= k) 
+                a[j] = a[j - k]; 
+            a[j] = temp; 
+        } 
+    }
+}
 
 int main() {
-    std::cout << "Ejercicio 05/03\n" << std::endl;
+    cout << "Ejercicio 08/04\n" << endl;
+    srand(time(NULL));
+    int a[30];
+    cout << "Antes: ";
+    for(int i = 0; i < 30; i++) {
+        a[i] = rand() % 400 + 100;
+        cout << a[i] << ' ';
+    }
+    shellSort(a, 30);
+    cout << endl << "Despues: ";
+    for(int i = 0; i < 30; i++)
+        cout << a[i] << ' ';
+    cout << endl;
+    
     return 0;
 }
