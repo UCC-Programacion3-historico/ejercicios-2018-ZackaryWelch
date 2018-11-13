8b3bdbc66301fb1049eb38fe17f1d66abc704fba
ZackaryWelch <welch.zackary@gmail.com>
2018-11-12 22:49:27 -0300

    Terminado con U08/Ej-02

--- U08_Ordenamiento/Ej-02/main.cpp
+++ U08_Ordenamiento/Ej-02/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void interSort(int *a, int n) {
+    for(int i = 1; i < n; i++) {
+        for(int j = i; j > 0 and a[j-1] > a[j]; j--) {
+            int temp = a[j];
+            a[j] = a[j-1];
+            a[j-1] = temp;
+        }
+    } 
+}
 
 int main() {
-    std::cout << "Ejercicio 05/02\n" << std::endl;
+    cout << "Ejercicio 08/02\n" << endl;
+    srand(time(NULL));
+    int a[30];
+    cout << "Antes: ";
+    for(int i = 0; i < 30; i++) {
+        a[i] = rand() % 400 + 100;
+        cout << a[i] << ' ';
+    }
+    interSort(a, 30);
+    cout << endl << "Despues: ";
+    for(int i = 0; i < 30; i++)
+        cout << a[i] << ' ';
+    cout << endl;
+    
     return 0;
 }
